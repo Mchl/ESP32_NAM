32b8ee7e79290eb4dc6ffbb2af9f7e2d8557096e
Michal Jarosz <mchlpl@gmail.com>
2021-05-29 00:51:10 +0200

    Connect to WiFi

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <Arduino.h>
+#include <WiFi.h>
+
+#include "secrets.h"
+
+void setupWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  WiFi.setHostname("ESP32_NAM_Display");
+
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+      Serial.print(".");
+      esp_restart();
+  }
+  Serial.println(F("WiFi connected"));
+  Serial.println("");
+  Serial.println(WiFi.localIP());
+}
 
 void setup() {
   Serial.begin(115200);
+  delay(5000);
+  setupWiFi();
 }
 
 void loop() {
---
+++ src/secrets.template.h
@@ -0,0 +1,4 @@
+// Fill this in with your WiFi credentials and change name/copy to secrets.h
+
+#define WIFI_SSID                       "SSID"
+#define WIFI_PASSWORD                   "WiFi Password"
