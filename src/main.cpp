d67f33ab7dd6d82eac24d8583a41c729fbcfb59e
Michal Jarosz <mchlpl@gmail.com>
2021-05-29 02:06:30 +0200

    Read data from remote server

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 
 #include "secrets.h"
 
+WiFiClient http;
+IPAddress namServer(192,168,1,38);
+
 void setupWiFi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -19,11 +22,44 @@ void setupWiFi() {
 
 void setup() {
   Serial.begin(115200);
-  delay(5000);
   setupWiFi();
 }
 
 void loop() {
-  delay(1000);
-  Serial.println("BLINK");
+  char status[32] = {0};
+
+  http.connect(namServer, 80);
+  http.println("GET /data.json HTTP/1.0");
+  http.println();
+  while (!http.available()) {
+    delay(10);
+  }
+
+  http.readBytesUntil('\r', status, sizeof(status));
+
+  if (strcmp(status, "HTTP/1.0 200 OK") != 0)
+  {
+    Serial.print("Unexpected response: ");
+    Serial.println(status);
+    http.stop();
+    return;
+  }
+
+// Skip HTTP headers
+  char endOfHeaders[] = "\r\n\r\n";
+  if (!http.find(endOfHeaders))
+  {
+    Serial.println("Invalid response");
+    http.stop();
+    return;
+  }
+
+  while (http.available()) {
+    char c = http.read();
+    Serial.print(c);
+  }
+  Serial.println();
+
+  http.stop();
+  delay(5000);
 }
