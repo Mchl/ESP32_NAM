c2e22ef747dbdf6c8fe4ab90c212e68d5d57f8b9
Michal Jarosz <michal.jarosz@stepstone.com>
2021-05-30 19:54:52 +0200

    Hello world the display

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
 #include <Arduino.h>
+#include <Adafruit_I2CDevice.h>
+#include <GxEPD2_BW.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+
+#include "GxEPD2_display_selection_new_style.h"
 #include "network.h"
 #include "parse_json.h"
+
 
 void setup() {
   Serial.begin(115200);
   setupWiFi();
+  display.init();
+  display.setRotation(1);
+  display.setFont(&FreeMonoBold9pt7b);
+  display.setTextColor(GxEPD_BLACK);
+  int16_t tbx, tby;
+  uint16_t tbw, tbh;
+  display.getTextBounds("Hello, World!", 0, 0, &tbx, &tby, &tbw, &tbh);
+  // center the bounding box by transposition of the origin:
+  uint16_t x = ((display.width() - tbw) / 2) - tbx;
+  uint16_t y = ((display.height() - tbh) / 2) - tby;
+  display.setFullWindow();
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(x, y);
+    display.print("Hello, World!");
+  } while (display.nextPage());
+  display.hibernate();
 }
 
 void loop() {
