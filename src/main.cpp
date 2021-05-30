a881580778338880ddf384881b6c42a1af60e3c8
Michal Jarosz <michal.jarosz@stepstone.com>
2021-05-30 22:02:43 +0200

    Display NAM values on screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <Adafruit_I2CDevice.h>
 #include <GxEPD2_BW.h>
-#include <Fonts/FreeMonoBold9pt7b.h>
+#include <Fonts/FreeMono9pt7b.h>
 
 #include "GxEPD2_display_selection_new_style.h"
 #include "network.h"
@@ -13,23 +13,9 @@ void setup() {
   setupWiFi();
   display.init();
   display.setRotation(1);
-  display.setFont(&FreeMonoBold9pt7b);
+  display.setFont(&FreeMono9pt7b);
   display.setTextColor(GxEPD_BLACK);
-  int16_t tbx, tby;
-  uint16_t tbw, tbh;
-  display.getTextBounds("Hello, World!", 0, 0, &tbx, &tby, &tbw, &tbh);
-  // center the bounding box by transposition of the origin:
-  uint16_t x = ((display.width() - tbw) / 2) - tbx;
-  uint16_t y = ((display.height() - tbh) / 2) - tby;
-  display.setFullWindow();
-  display.firstPage();
-  do
-  {
-    display.fillScreen(GxEPD_WHITE);
-    display.setCursor(x, y);
-    display.print("Hello, World!");
-  } while (display.nextPage());
-  display.hibernate();
+
 }
 
 void loop() {
@@ -42,14 +28,19 @@ void loop() {
 
   NamData namData = parseJson(dataFromNam);
 
-  Serial.printf(
-      "PM 10: %.1f%%  PM 2.5: %.1f%%  Temperature: %.1f °C  Pressure: %.1f hPa  Humidity: %.1f%%  Signal: %d dBm \n",
-      namData.PM10percentage,
-      namData.PM2_5percentage,
-      namData.temperature,
-      namData.pressureBarometric,
-      namData.humidity,
-      namData.signal);
+  display.setFullWindow();
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(0, 35);
+    display.print(String("  PM 10: ") + namData.PM10percentage + String("%\n"));
+    display.print(String(" PM 2.5: ") + namData.PM2_5percentage + String("%\n"));
+    display.print(String("   Temp: ") + namData.temperature + String("C\n"));
+    display.print(String("    Hum: ") + namData.humidity + String("%\n"));
+    display.print(String("   Pres: ") + namData.pressureBarometric + String("hPa"));
+  } while (display.nextPage());
+  display.hibernate();
 
   if (wait) {
     delay(5000); // execute once every 5 seconds, don't flood remote service
